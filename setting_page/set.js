c20b68ba0059c1a398c3aadac59819725b6313dc
for-the-zero <ourmicroid@outlook.com>
2024-08-27 17:07:03 +0800

    func: settings init

--- setting_page/set.js
+++ setting_page/set.js
@@ -1,5 +1,5 @@
 const { ipcRenderer } = require('electron');
-const { setColorScheme, setTheme } = require('../src/mdui.cjs');
+const { setColorScheme, setTheme, prompt } = require('../src/mdui.cjs');
 const $ = require('jquery');
 
 //setColorScheme('#5577FF');
@@ -39,6 +39,13 @@ const ele_up_icon = $('.up-icon');
 const ele_up_if = $('.up-if');
 const ele_up_command = $('.up-cmd');
 const ele_up_url = $('.up-url');
+const ele_sspath_span = $('.ss-path > span');
+const ele_sspath_btn = $('.ss-path > mdui-button-icon');
+const ele_gtm_span = $('.gtm-url > span');
+const ele_gtm_btn = $('.gtm-url > mdui-button-icon');
+const ele_openss = $('.open-ss');
+const ele_opentl = $('.open-tl');
+const ele_opencr = $('.open-cr');
 
 
 $('.about-btn').on('click',()=>{
@@ -51,7 +58,7 @@ ele_lang_opt.on('click',()=>{
 });
 ele_lm_opt.on('click',()=>{
     if (ele_lm_opt.val() === ''){
-        ele_lm_opt.val('light');
+        ele_lm_opt.val('auto');
     };
 });
 ele_pp_opt.on('click',()=>{
@@ -67,11 +74,11 @@ ele_cqi_addbtn.on('click',()=>{
         //let html = `<mdui-list-item nonclickable>${val}<mdui-button-icon slot="end-icon" icon="close"></mdui-button-icon></mdui-list-item>`;
         let html = `<mdui-list-item nonclickable>${val}</mdui-list-item>`;
         let del_btn = `<mdui-button-icon slot="end-icon" icon="close"></mdui-button-icon>`;
-        del_btn = $(del_btn)
+        del_btn = $(del_btn);
         del_btn.on('click',function(){
             $(this).parent().remove();
         });
-        html = $(html).append(del_btn)
+        html = $(html).append(del_btn);
         ele_cqi_list.append(html);
         ele_cqi_input.val('');
     };
@@ -83,11 +90,11 @@ ele_cjsp_addbtn.on('click',()=>{
         //let html = `<mdui-list-item nonclickable>${val}<mdui-button-icon slot="end-icon" icon="close"></mdui-button-icon></mdui-list-item>`;
         let html = `<mdui-list-item nonclickable>${val}</mdui-list-item>`;
         let del_btn = `<mdui-button-icon slot="end-icon" icon="close"></mdui-button-icon>`;
-        del_btn = $(del_btn)
+        del_btn = $(del_btn);
         del_btn.on('click',function(){
             $(this).parent().remove();
         });
-        html = $(html).append(del_btn)
+        html = $(html).append(del_btn);
         ele_cjsp_list.append(html);
         ele_cjsp_input.val('');
     };
@@ -134,6 +141,99 @@ ele_up_type.on('change',()=>{
     };
 });
 
-function init_config(){} //TODO:
+// edit path btns
+ele_sspath_btn.on('click',()=>{
+    prompt({
+        headline: config.app_settings.Chinese ? '填写截图保存路径' : 'Enter screenshot save path',
+        description: config.app_settings.Chinese ? '留空则在截图后弹出窗口选择，另外我懒得在这写检测了' : 'Leave it blank to select the path after taking a screenshot, and lazy to write the detection code',
+        confirmText: config.app_settings.Chinese ? '确定' : 'Confirm',
+        cancelText: config.app_settings.Chinese ? '取消' : 'Cancel',
+        onConfirm: (path) => {
+            ele_sspath_span.text(path);
+        },
+        onCancel: () => {console.log('canceled')}, // 你妈的Cancel为什么还要Reject一下啊
+    });
+});
+ele_gtm_btn.on('click',()=>{
+    prompt({
+        headline: config.app_settings.Chinese ? '填写谷歌翻译镜像网站链接' : 'Enter Google Translate Mirror URL',
+        description: config.app_settings.Chinese ? '留空则不启用该功能' : 'Leave it blank to disable this feature',
+        confirmText: config.app_settings.Chinese ? '确定' : 'Confirm',
+        cancelText: config.app_settings.Chinese ? '取消' : 'Cancel',
+        onConfirm: (url) => {
+            ele_gtm_span.text(url);
+        },
+        onCancel: () => {console.log('canceled')},
+    });
+});
 
-ele_savebtn.on('click',() => {}); //TODO:
+function init_config(){
+    ele_lang_opt.val(config.app_settings.Chinese ? 'true' : 'false');
+    //ele_lm_opt.val(config.app_settings.light_dark);
+    if (config.app_settings.light_dark == ''){
+        ele_lm_opt.val('auto');
+    }else{
+        ele_lm_opt.val(config.app_settings.light_dark);
+    };
+    ele_pp_opt.val(config.app_settings.panel_place);
+    ele_sspath_span.text(config.default_plugin_settings.screenshot_save_path);
+    ele_gtm_span.text(config.default_plugin_settings.google_translate_mirror);
+    if(config.default_tools.includes('screenshot')){
+        ele_openss.attr('checked',true);
+    };
+    if(config.default_tools.includes('translate')){
+        ele_opentl.attr('checked',true);
+    };
+    if(config.default_tools.includes('cliprecog')){
+        ele_opencr.attr('checked',true);
+    };
+
+    for (let i = 0; i < config.default_plugin_settings.clip_quickinput.length; i++){
+        let val = config.default_plugin_settings.clip_quickinput[i];
+        let html = `<mdui-list-item nonclickable>${val}</mdui-button-icon></mdui-list-item>`;
+        let del_btn = `<mdui-button-icon slot="end-icon" icon="close"></mdui-button-icon>`;
+        del_btn = $(del_btn);
+        del_btn.on('click',function(){
+            $(this).parent().remove();
+        });
+        html = $(html).append(del_btn);
+        ele_cqi_list.append(html);
+    };
+    for (let i = 0; i < config.plugin_tools.length; i++){
+        let val = config.plugin_tools[i];
+        let html = `<mdui-list-item nonclickable>${val}</mdui-button-icon></mdui-list-item>`;
+        let del_btn = `<mdui-button-icon slot="end-icon" icon="close"></mdui-button-icon>`;
+        del_btn = $(del_btn);
+        del_btn.on('click',function(){
+            $(this).parent().remove();
+        });
+        html = $(html).append(del_btn)
+        ele_cjsp_list.append(html);
+    };
+    for (let i = 0; i < config.custom_tools.length; i++){
+        let val = config.custom_tools[i];
+        if (val.type === 'cmd'){
+            ele_up_tbody.append(`<tr>
+                <td>cmd</td>
+                <td>${val.name}</td>
+                <td>${val.icon}</td>
+                <td>${val.if}</td>
+                <td>${val.command}</td>
+                <td></td>
+            </tr>`);
+        } else {
+            ele_up_tbody.append(`<tr>
+                <td>pwa</td>
+                <td>${val.name}</td>
+                <td>${val.icon}</td>
+                <td></td>
+                <td></td>
+                <td>${val.url}</td>
+            </tr>`);
+        };
+    };
+};
+
+ele_savebtn.on('click',() => {
+    //TODO:
+});
